19e590b079710dec72bd52b76430babdeed305e2
Syphax bouazzouni <gs_bouazzouni@esi.dz>
2024-03-30 06:27:30 +0100

    fix ruby treesitter indent

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,10 +2,16 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
+-- Fix the ruby indent bug 
+vim.cmd('autocmd FileType ruby setlocal indentkeys-=.')
+
 vim.g.mapleader = " "
 vim.g.background = "light"
 
 vim.opt.swapfile = false
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 -- Navigate vim panes better
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
@@ -13,6 +19,4 @@ vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
-vim.wo.number = true
-
+--vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
